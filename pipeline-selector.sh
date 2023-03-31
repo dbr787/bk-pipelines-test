204804161d4689f50725b6b7f3cad29e67158a48
Daniel Ring <dbr787@gmail.com>
2023-03-31 15:47:41 +1100

    add pipeline yamls

--- pipeline-selector.sh
+++ pipeline-selector.sh
@@ -2,16 +2,16 @@
 
 set -euo pipefail
 
-ls -la
-ls -la ./pipelines
-echo "BUILDKITE_REPO is $BUILDKITE_REPO"
+# ls -la
+# ls -la ./pipelines
+# echo "BUILDKITE_REPO is $BUILDKITE_REPO"
 # echo "$BUILDKITE_REPO" | sed 's|.*/||'
 PIPELINE_DIR=$(echo "$BUILDKITE_REPO" | cut -d / -f 4-)
-echo "PIPELINE_DIR is $PIPELINE_DIR"
+# echo "PIPELINE_DIR is $PIPELINE_DIR"
 FULL_PIPELINE_DIR="./pipelines/$PIPELINE_DIR"
-echo "FULL_PIPELINE_DIR is $FULL_PIPELINE_DIR"
-ls -l $FULL_PIPELINE_DIR
+# echo "FULL_PIPELINE_DIR is $FULL_PIPELINE_DIR"
+# ls -l $FULL_PIPELINE_DIR
 PIPELINE_FILE="$FULL_PIPELINE_DIR/pipeline.yaml"
-echo "PIPELINE_FILE is $PIPELINE_FILE"
+# echo "PIPELINE_FILE is $PIPELINE_FILE"
 
 buildkite-agent pipeline upload $PIPELINE_FILE
