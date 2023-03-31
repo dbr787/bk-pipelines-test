ecb2c71431e46eaeb95958521b187507a5b4983f
Daniel Ring <dbr787@gmail.com>
2023-03-31 18:53:13 +1100

    work on getting pipeline source

--- pipeline-selector.sh
+++ pipeline-selector.sh
@@ -4,19 +4,15 @@ set -euo pipefail
 
 # ls -la
 # ls -la ./pipelines
-# echo "BUILDKITE_REPO is $BUILDKITE_REPO"
-# echo "$BUILDKITE_REPO" | sed 's|.*/||'
+echo "BUILDKITE_REPO is $BUILDKITE_REPO"
+echo "$BUILDKITE_REPO" | sed 's|.*/||'
 PIPELINE_DIR=$(echo "$BUILDKITE_REPO" | cut -d / -f 4- | sed 's/\.git//')
-# echo "PIPELINE_DIR is $PIPELINE_DIR"
+echo "PIPELINE_DIR is $PIPELINE_DIR"
 FULL_PIPELINE_DIR="./pipelines/$PIPELINE_DIR"
-# echo "FULL_PIPELINE_DIR is $FULL_PIPELINE_DIR"
-# ls -l $FULL_PIPELINE_DIR
+echo "FULL_PIPELINE_DIR is $FULL_PIPELINE_DIR"
+ls -l $FULL_PIPELINE_DIR
 PIPELINE_FILE="$FULL_PIPELINE_DIR/pipeline.yaml"
-# echo "PIPELINE_FILE is $PIPELINE_FILE"
-
-# echo "$BUILDKITE_PLUGINS" | jq '.'
-
-# echo "$BUILDKITE_PLUGINS" | jq -r '.[] | with_entries(select(.key|contains("hasura/smooth-checkout-buildkite-plugin")))[].repos[].config[].url'
+echo "PIPELINE_FILE is $PIPELINE_FILE"
 
 # Get base repository from plugin config
 PIPELINE_REPO=$(echo "$BUILDKITE_PLUGINS" | jq -r '.[] | with_entries(select(.key|contains("hasura/smooth-checkout-buildkite-plugin")))[].repos[].config[].url' | sed 's/\.git//')
