525ee595c40abeb9ecfde847692ca6a0c868224a
Daniel Ring <dbr787@gmail.com>
2023-03-31 15:13:28 +1100

    get folder from url

--- pipeline-selector.sh
+++ pipeline-selector.sh
@@ -6,6 +6,6 @@ ls -la
 ls -la ./pipelines
 echo "$BUILDKITE_REPO"
 echo "$BUILDKITE_REPO" | sed 's|.*/||'
-echo "$BUILDKITE_REPO" | cut -d / -f 3-
+echo "$BUILDKITE_REPO" | cut -d / -f 4-
 MYVAR=$(echo "$BUILDKITE_REPO" | sed 's|.*/||')
 echo $MYVAR
