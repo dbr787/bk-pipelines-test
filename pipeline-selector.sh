d87a3fa3f4d4026eab278b918b286f56e470f9d6
Daniel Ring <dbr787@gmail.com>
2023-03-31 19:14:50 +1100

    work on getting pipeline source

--- pipeline-selector.sh
+++ pipeline-selector.sh
@@ -28,8 +28,8 @@ buildkite-agent annotate "Pipeline\ndefinition\\nuploaded\\\nfrom our central re
 # buildkite-agent annotate "Pipeline URL: $PIPELINE_URL\n" --style 'info' --context 'ctx-more'
 # buildkite-agent annotate "Pipeline REF: $PIPELINE_REF" --style 'info' --context 'ctx-more' --append
 
-echo -e "ekho e Pipeline\\nURL: $PIPELINE_URL\nPipeline\\\nREF: $PIPELINE_REF  \n 1  \\n 2  \\\n 3" | buildkite-agent annotate --style 'info' --context 'ctx-more1'
-printf "printf Pipeline\\nURL: $PIPELINE_URL\nPipeline\\\nREF: $PIPELINE_REF  \n 1  \\n 2  \\\n 3" | buildkite-agent annotate --style 'info' --context 'ctx-more2'
+echo -e "Pipeline URL: $PIPELINE_URL  \nPipeline REF: $PIPELINE_REF" | buildkite-agent annotate --style 'info' --context 'ctx-more1'
+# printf "printf Pipeline\\nURL: $PIPELINE_URL\nPipeline\\\nREF: $PIPELINE_REF  \n 1  \\n 2  \\\n 3" | buildkite-agent annotate --style 'info' --context 'ctx-more2'
 
 
 
