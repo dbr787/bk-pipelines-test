af07991a63810579cd2e9c143fc7d4018a4440ca
Daniel Ring <dbr787@gmail.com>
2023-03-31 15:19:28 +1100

    get folder from url

--- pipeline-selector.sh
+++ pipeline-selector.sh
@@ -6,7 +6,7 @@ ls -la
 ls -la ./pipelines
 echo "BUILDKITE_REPO is $BUILDKITE_REPO"
 # echo "$BUILDKITE_REPO" | sed 's|.*/||'
-PIPELINE_DIR=$(echo "$BUILDKITE_REPO" | cut -d / -f 4-)
+PIPELINE_DIR=$(echo "$BUILDKITE_REPO"/ | cut -d / -f 4-)
 echo "PIPELINE_DIR is $PIPELINE_DIR"
-FULL_PIPELINE_DIR="pipelines/$PIPELINE_DIR"
+FULL_PIPELINE_DIR="pipelines/$PIPELINE_DIR/"
 echo "FULL_PIPELINE_DIR is $FULL_PIPELINE_DIR"
