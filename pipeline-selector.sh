92a68c28acba9470f70c7066388ae98fb4bf464b
Daniel Ring <dbr787@gmail.com>
2023-03-31 15:34:00 +1100

    get folder from url

--- pipeline-selector.sh
+++ pipeline-selector.sh
@@ -6,8 +6,11 @@ ls -la
 ls -la ./pipelines
 echo "BUILDKITE_REPO is $BUILDKITE_REPO"
 # echo "$BUILDKITE_REPO" | sed 's|.*/||'
-PIPELINE_DIR=$(echo "$BUILDKITE_REPO"/ | cut -d / -f 4-)
+PIPELINE_DIR=$(echo "$BUILDKITE_REPO" | cut -d / -f 4-)
 echo "PIPELINE_DIR is $PIPELINE_DIR"
 FULL_PIPELINE_DIR="./pipelines/$PIPELINE_DIR"
 echo "FULL_PIPELINE_DIR is $FULL_PIPELINE_DIR"
-ls -la $FULL_PIPELINE_DIR
+ls -l $FULL_PIPELINE_DIR
+PIPELINE_FILE="$FULL_PIPELINE_DIR/pipeline.yaml"
+echo "PIPELINE_FILE is $PIPELINE_FILE"
+# buildkite-agent pipeline upload pipeline.yaml
