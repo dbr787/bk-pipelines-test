33e742e57a62bdd39937a438cf72f7776ba62097
Daniel Ring <dbr787@gmail.com>
2023-03-31 16:41:44 +1100

    try jq

--- pipeline-selector.sh
+++ pipeline-selector.sh
@@ -2,21 +2,19 @@
 
 set -euo pipefail
 
-ls -la
-ls -la ./pipelines
-echo "BUILDKITE_REPO is $BUILDKITE_REPO"
+# ls -la
+# ls -la ./pipelines
+# echo "BUILDKITE_REPO is $BUILDKITE_REPO"
 # echo "$BUILDKITE_REPO" | sed 's|.*/||'
 PIPELINE_DIR=$(echo "$BUILDKITE_REPO" | cut -d / -f 4- | sed 's/.git//')
-echo "PIPELINE_DIR is $PIPELINE_DIR"
+# echo "PIPELINE_DIR is $PIPELINE_DIR"
 FULL_PIPELINE_DIR="./pipelines/$PIPELINE_DIR"
-echo "FULL_PIPELINE_DIR is $FULL_PIPELINE_DIR"
-ls -l $FULL_PIPELINE_DIR
+# echo "FULL_PIPELINE_DIR is $FULL_PIPELINE_DIR"
+# ls -l $FULL_PIPELINE_DIR
 PIPELINE_FILE="$FULL_PIPELINE_DIR/pipeline.yaml"
-echo "PIPELINE_FILE is $PIPELINE_FILE"
+# echo "PIPELINE_FILE is $PIPELINE_FILE"
 
-echo "$BUILDKITE_PLUGINS"
-
-echo "$BUILDKITE_PLUGINS" | jq
+echo "$BUILDKITE_PLUGINS" | jq '.'
 
 buildkite-agent annotate "Pipeline definition uploaded from our central repository: $BUILDKITE_REPO" --style 'info' --context 'ctx-pipeline-selector'
 
