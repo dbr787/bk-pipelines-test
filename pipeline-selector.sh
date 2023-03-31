004b689795d9af742b64e3fc97570d3a9ae765ed
Daniel Ring <dbr787@gmail.com>
2023-03-31 16:27:06 +1100

    add annotation

--- pipeline-selector.sh
+++ pipeline-selector.sh
@@ -2,18 +2,18 @@
 
 set -euo pipefail
 
-# ls -la
-# ls -la ./pipelines
-# echo "BUILDKITE_REPO is $BUILDKITE_REPO"
-# echo "$BUILDKITE_REPO" | sed 's|.*/||'
+ls -la
+ls -la ./pipelines
+echo "BUILDKITE_REPO is $BUILDKITE_REPO"
+echo "$BUILDKITE_REPO" | sed 's|.*/||'
 PIPELINE_DIR=$(echo "$BUILDKITE_REPO" | cut -d / -f 4-)
-# echo "PIPELINE_DIR is $PIPELINE_DIR"
+echo "PIPELINE_DIR is $PIPELINE_DIR"
 FULL_PIPELINE_DIR="./pipelines/$PIPELINE_DIR"
-# echo "FULL_PIPELINE_DIR is $FULL_PIPELINE_DIR"
-# ls -l $FULL_PIPELINE_DIR
+echo "FULL_PIPELINE_DIR is $FULL_PIPELINE_DIR"
+ls -l $FULL_PIPELINE_DIR
 PIPELINE_FILE="$FULL_PIPELINE_DIR/pipeline.yaml"
-# echo "PIPELINE_FILE is $PIPELINE_FILE"
+echo "PIPELINE_FILE is $PIPELINE_FILE"
 
-buildkite-agent annotate "Pipeline uploaded from central repository: $BUILDKITE_REPO" --style 'info' --context 'ctx-pipeline-selector'
+buildkite-agent annotate "Pipeline definition uploaded from our central repository: $BUILDKITE_REPO" --style 'info' --context 'ctx-pipeline-selector'
 
 buildkite-agent pipeline upload $PIPELINE_FILE
