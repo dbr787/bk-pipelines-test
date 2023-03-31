020ff7a242935d9a560d4d2b107dfc353a7acd26
Daniel Ring <dbr787@gmail.com>
2023-03-31 15:00:40 +1100

    move selector script

--- pipeline-selector.sh
+++ pipeline-selector.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 ls -la
 ls -la ./pipelines
-echo "$$BUILDKITE_REPO"
-echo "$$BUILDKITE_REPO" | sed 's|.*/||'
-MYVAR=$(echo "$$BUILDKITE_REPO" | sed 's|.*/||')
+echo "$BUILDKITE_REPO"
+echo "$BUILDKITE_REPO" | sed 's|.*/||'
+MYVAR=$(echo "$BUILDKITE_REPO" | sed 's|.*/||')
 echo $MYVAR
