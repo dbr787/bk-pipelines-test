d1647e75c2cbc66bc1845853bfaf3e41a31cc7ac
Daniel Ring <dbr787@gmail.com>
2023-03-31 19:37:59 +1100

    work on getting pipeline source

--- pipeline-selector.sh
+++ pipeline-selector.sh
@@ -2,15 +2,27 @@
 
 set -euo pipefail
 
+PIPELINE_FILENAME='pipeline.yaml'
+PIPELINE_DIRNAME=$(echo "$BUILDKITE_REPO" | cut -d / -f 4- | sed 's/\.git$//')
+
+# LOCAL_PIPELINE_FILENAME=
+
+echo $PIPELINE_DIRNAME
+echo $PIPELINE_FILENAME
+
+
+
 # ls -la
 # ls -la ./pipelines
 echo "BUILDKITE_REPO is $BUILDKITE_REPO"
-echo "$BUILDKITE_REPO" | sed 's|.*/||'
+# echo "$BUILDKITE_REPO" | sed 's|.*/||'
 PIPELINE_DIR=$(echo "$BUILDKITE_REPO" | cut -d / -f 4- | sed 's/\.git$//')
 echo "PIPELINE_DIR is $PIPELINE_DIR"
+
 FULL_PIPELINE_DIR="./pipelines/$PIPELINE_DIR"
 echo "FULL_PIPELINE_DIR is $FULL_PIPELINE_DIR"
 ls -l $FULL_PIPELINE_DIR
+
 PIPELINE_FILE="$FULL_PIPELINE_DIR/pipeline.yaml"
 echo "PIPELINE_FILE is $PIPELINE_FILE"
 
