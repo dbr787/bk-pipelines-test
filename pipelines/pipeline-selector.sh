cf8290475cef4240ad01899bcb527c7068c8856d
Daniel Ring <dbr787@gmail.com>
2023-03-31 14:55:36 +1100

    add selector script

---
+++ pipelines/pipeline-selector.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -euo pipefail
+
+ls -la
+ls -la ./pipelines
+echo "$$BUILDKITE_REPO"
+echo "$$BUILDKITE_REPO" | sed 's|.*/||'
+MYVAR=$(echo "$$BUILDKITE_REPO" | sed 's|.*/||')
+echo $MYVAR
