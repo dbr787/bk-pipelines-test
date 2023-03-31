e81c79373091eacfec88774dbf9c631c3b2489fb
Daniel Ring <dbr787@gmail.com>
2023-03-31 17:05:49 +1100

    try jq

--- pipeline-selector.sh
+++ pipeline-selector.sh
@@ -14,7 +14,10 @@ FULL_PIPELINE_DIR="./pipelines/$PIPELINE_DIR"
 PIPELINE_FILE="$FULL_PIPELINE_DIR/pipeline.yaml"
 # echo "PIPELINE_FILE is $PIPELINE_FILE"
 
-echo "$BUILDKITE_PLUGINS" | jq '.'
+# echo "$BUILDKITE_PLUGINS" | jq '.'
+echo "$BUILDKITE_PLUGINS" | jq '.[] | with_entries(select(.key|contains("hasura/smooth-checkout-buildkite-plugin")))[].repos[].config[].url'
+
+# .[] | with_entries( select(.key|contains("hasura/smooth-checkout-buildkite-plugin")))
 
 buildkite-agent annotate "Pipeline definition uploaded from our central repository: $BUILDKITE_REPO" --style 'info' --context 'ctx-pipeline-selector'
 
