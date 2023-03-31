edb716ad4fc0d37cc9e3b8177d72724e7e7a30b5
Daniel Ring <dbr787@gmail.com>
2023-03-31 20:00:45 +1100

    work on getting pipeline source

--- pipeline-selector.sh
+++ pipeline-selector.sh
@@ -2,41 +2,28 @@
 
 set -euo pipefail
 
+# Get project repository
 PIPELINE_FILENAME='pipeline.yaml'
 PIPELINE_ORG_REPO=$(echo "$BUILDKITE_REPO" | cut -d / -f 4- | sed 's/\.git$//')
+
+# Construct local pipeline file path
 LOCAL_PIPELINE_PATH="./pipelines/$PIPELINE_ORG_REPO/$PIPELINE_FILENAME"
-
-echo $PIPELINE_ORG_REPO
-echo $PIPELINE_FILENAME
-echo $LOCAL_PIPELINE_PATH
-
-
-# ls -la
-# ls -la ./pipelines
-# echo "BUILDKITE_REPO is $BUILDKITE_REPO"
-# # echo "$BUILDKITE_REPO" | sed 's|.*/||'
-# PIPELINE_DIR=$(echo "$BUILDKITE_REPO" | cut -d / -f 4- | sed 's/\.git$//')
-# echo "PIPELINE_DIR is $PIPELINE_DIR"
-
-# FULL_PIPELINE_DIR="./pipelines/$PIPELINE_DIR"
-# echo "FULL_PIPELINE_DIR is $FULL_PIPELINE_DIR"
-# ls -l $FULL_PIPELINE_DIR
-
-# PIPELINE_FILE="$FULL_PIPELINE_DIR/pipeline.yaml"
-# echo "PIPELINE_FILE is $PIPELINE_FILE"
-
+echo "PIPELINE_ORG_REPO is $PIPELINE_ORG_REPO"
+echo "PIPELINE_FILENAME is $PIPELINE_FILENAME"
+echo "LOCAL_PIPELINE_PATH is $LOCAL_PIPELINE_PATH"
 
 # Get pipeline url and ref from plugin config
 PIPELINE_URL=$(echo "$BUILDKITE_PLUGINS" | jq -r '.[] | with_entries(select(.key|contains("hasura/smooth-checkout-buildkite-plugin")))[].repos[].config[].url' | sed 's/\.git$//')
 PIPELINE_REF=$(echo "$BUILDKITE_PLUGINS" | jq -r '.[] | with_entries(select(.key|contains("hasura/smooth-checkout-buildkite-plugin")))[].repos[].config[].ref' | sed 's/\.git$//')
-REMOTE_PIPELINE_FILE="$PIPELINE_URL/tree/$PIPELINE_REF/pipelines/$PIPELINE_DIR/pipeline.yaml"
+echo "PIPELINE_URL is $PIPELINE_URL"
+echo "PIPELINE_REF is $PIPELINE_REF"
 
-# https://github.com/dbr787/bk-pipelines-test/tree/main/pipelines/dbr787/bk-project-test-1
+# Construct remote pipeline url
+REMOTE_PIPELINE_FILE="$PIPELINE_URL/tree/$PIPELINE_REF/pipelines/$PIPELINE_ORG_REPO/pipeline.yaml"
+echo "REMOTE_PIPELINE_FILE is $REMOTE_PIPELINE_FILE"
 
-echo $PIPELINE_URL
-echo $PIPELINE_REF
-
-
+# Create annotation
 echo -e "Pipeline Definition [here]('$REMOTE_PIPELINE_FILE'): $REMOTE_PIPELINE_FILE  \nPipeline URL: $PIPELINE_URL  \nPipeline REF: \`$PIPELINE_REF\`" | buildkite-agent annotate --style 'info' --context 'ctx-more2'
 
+# Upload pipeline
 buildkite-agent pipeline upload $LOCAL_PIPELINE_PATH
