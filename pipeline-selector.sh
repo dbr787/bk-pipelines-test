50e5691954bbf66007e2f3ec7dae3d32361b27e4
Daniel Ring <dbr787@gmail.com>
2023-03-31 16:32:23 +1100

    remove .git from string

--- pipeline-selector.sh
+++ pipeline-selector.sh
@@ -5,8 +5,8 @@ set -euo pipefail
 ls -la
 ls -la ./pipelines
 echo "BUILDKITE_REPO is $BUILDKITE_REPO"
-echo "$BUILDKITE_REPO" | sed 's|.*/||'
-PIPELINE_DIR=$(echo "$BUILDKITE_REPO" | cut -d / -f 4-)
+# echo "$BUILDKITE_REPO" | sed 's|.*/||'
+PIPELINE_DIR=$(echo "$BUILDKITE_REPO" | cut -d / -f 4- | sed 's/.git//')
 echo "PIPELINE_DIR is $PIPELINE_DIR"
 FULL_PIPELINE_DIR="./pipelines/$PIPELINE_DIR"
 echo "FULL_PIPELINE_DIR is $FULL_PIPELINE_DIR"
