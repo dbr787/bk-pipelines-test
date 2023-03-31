27b938458b173d481c155ac6e12517efdc72c070
Daniel Ring <dbr787@gmail.com>
2023-03-31 18:56:52 +1100

    work on getting pipeline source

--- pipeline-selector.sh
+++ pipeline-selector.sh
@@ -14,11 +14,18 @@ ls -l $FULL_PIPELINE_DIR
 PIPELINE_FILE="$FULL_PIPELINE_DIR/pipeline.yaml"
 echo "PIPELINE_FILE is $PIPELINE_FILE"
 
-# Get base repository from plugin config
-PIPELINE_REPO=$(echo "$BUILDKITE_PLUGINS" | jq -r '.[] | with_entries(select(.key|contains("hasura/smooth-checkout-buildkite-plugin")))[].repos[].config[].url' | sed 's/\.git//')
-echo $PIPELINE_REPO
+# PIPELINE_URL
+# PIPELINE_REF
 
-buildkite-agent annotate "Pipeline definition uploaded from our central repository: $BUILDKITE_REPO" --style 'info' --context 'ctx-pipeline-selector'
-buildkite-agent annotate " $PIPELINE_REPO" --style 'info' --context 'ctx-more'
+# Get pipeline url and ref from plugin config
+PIPELINE_URL=$(echo "$BUILDKITE_PLUGINS" | jq -r '.[] | with_entries(select(.key|contains("hasura/smooth-checkout-buildkite-plugin")))[].repos[].config[].url' | sed 's/\.git//')
+PIPELINE_REF=$(echo "$BUILDKITE_PLUGINS" | jq -r '.[] | with_entries(select(.key|contains("hasura/smooth-checkout-buildkite-plugin")))[].repos[].config[].ref' | sed 's/\.git//')
+
+echo $PIPELINE_URL
+echo $PIPELINE_REF
+
+buildkite-agent annotate "Pipeline definition uploaded from our central repository: $PIPELINE_URL" --style 'info' --context 'ctx-pipeline-selector'
+buildkite-agent annotate "Pipeline URL: $PIPELINE_URL" --style 'info' --context 'ctx-more'
+buildkite-agent annotate "Pipeline REF: $PIPELINE_REF" --style 'info' --context 'ctx-more' --append
 
 buildkite-agent pipeline upload $PIPELINE_FILE
